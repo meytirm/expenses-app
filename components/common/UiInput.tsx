a1d3388e7b0793c3099aa7c75de367d1019bd415
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-14 13:32:05 +0330

    feat(components): add reusable UiInput component
    Implemented a `UiInput` component in `common` for text input with styling support. Includes label rendering and multiline option. Streamlines input handling across screens with consistent styles.

---
+++ components/common/UiInput.tsx
@@ -0,0 +1,52 @@
+import {StyleSheet, Text, TextInput, TextInputProps, View} from "react-native";
+import {GlobalStyles} from "../../constants/styles";
+
+function UiInput({textInputConfig}: Props) {
+
+  const inputStyles: object[] = [styles.input]
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline)
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{textInputConfig?.label}</Text>
+      <TextInput
+        style={inputStyles}
+        {...textInputConfig}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 16,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: 'top',
+  }
+})
+
+export default UiInput
+
+interface Props {
+  textInputConfig?: TextInputProps & {
+    label: string
+  }
+}
