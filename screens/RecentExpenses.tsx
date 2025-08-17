11d85339d738847c3899a9dce762d1adf109c507
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-17 16:13:44 +0330

    feat(screens): fetch expenses directly in RecentExpenses with state handling
    Integrated loading state and `fetchExpenses` logic directly into the `RecentExpenses` screen, replacing reliance on context. Improved error handling and ensured expenses are fetched and filtered for the last 7 days during component mount.

--- screens/RecentExpenses.tsx
+++ screens/RecentExpenses.tsx
@@ -1,15 +1,18 @@
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import {ExpensesContext} from "../store/expenses/expenses-context";
 import {getDateMinusDays} from "../utils/date";
 import LoadingOverlay from "../components/common/LoadingOverlay";
+import {ExpenseInterface} from "../types/expense";
+import {findAllExpenses} from "../service/expense";
 
 function RecentExpenses() {
+  const [loading, setLoading] = useState(false)
   const expensesContext = useContext(ExpensesContext)
   if (!expensesContext) {
     throw new Error('ExpensesContext is not provided')
   }
-  const {expenses, loading} = expensesContext
+  const {expenses, readExpense} = expensesContext
   const recentExpenses = expenses
     .filter((expense) => {
       const today = new Date()
@@ -17,6 +20,33 @@ function RecentExpenses() {
 
       return expense.date > dateSevenDaysAgo
     })
+
+  useEffect(() => {
+    async function fetchExpenses() {
+      setLoading(true)
+      try {
+        const expensesMapped: ExpenseInterface[] = []
+        const response = await findAllExpenses()
+        Object.keys(response.data).forEach(key => {
+          const expense: ExpenseInterface = {
+            id: key,
+            date: new Date(response.data[key].date),
+            description: response.data[key].description,
+            amount: response.data[key].amount,
+          }
+          expensesMapped.push(expense)
+
+          readExpense(expensesMapped)
+        })
+      } catch (e) {
+        console.log(e)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchExpenses()
+  }, []);
 
   if (loading) {
     return <LoadingOverlay />
