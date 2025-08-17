29e42b9ac33f7b3f8da10a06c58a6aa71a0cfd6a
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-17 09:19:18 +0330

    feat(service): add HTTP request utility with axios
    Implemented `http.ts` with `axios` for reusable HTTP request handling. Added `get` and `post` functions to standardize API interaction and improve maintainability. Configured default Axios instance with base URL, timeout, and headers.

---
+++ service/http.ts
@@ -0,0 +1,18 @@
+import axios, {AxiosRequestConfig} from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+})
+
+export function get<ResponseType>(url: string, config: AxiosRequestConfig = {}) {
+  return axiosInstance.get<ResponseType>(url, config)
+}
+
+export function post<ResponseType, RequestType>(url: string, data: RequestType, config: AxiosRequestConfig = {}) {
+  return axiosInstance.post<ResponseType>(url, data, config)
+}
