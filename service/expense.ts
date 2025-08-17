6fe9ac909139c80009142b789cf64655e283cf4d
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-17 13:06:17 +0330

    feat(service): add findAllExpenses function and enhance createExpense typings
    Introduced `findAllExpenses` function to retrieve all expenses from the `/expenses.json` endpoint. Updated `createExpense` function with generic typings for improved type safety and consistency.

--- service/expense.ts
+++ service/expense.ts
@@ -1,6 +1,11 @@
-import {post} from "./http";
-import {ExpenseInterface} from "../types/expense";
+import {get, post} from "./http";
+import {ExpenseCreateResponse, ExpenseInterface} from "../types/expense";
+import {AxiosResponse} from "axios";
 
 export function createExpense(expense: Omit<ExpenseInterface, 'id'>) {
-  return post('https://expense-app-bc386-default-rtdb.firebaseio.com/expenses.json', expense)
+  return post<ExpenseCreateResponse, Omit<ExpenseInterface, 'id'>>('/expenses.json', expense)
+}
+
+export function findAllExpenses() {
+  return get('/expenses.json')
 }
