ed5e5a39e45bde96de814e30b56b0efdb4a052f9
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-17 14:48:18 +0330

    feat(context): add loading state and fetchExpenses logic to ExpensesContextProvider
    Integrated a `loading` state and `fetchExpenses` function within `ExpensesContextProvider` to manage expense data retrieval from the service layer. Ensures asynchronous data fetching and provides better state handling with the addition of a loading indicator.

--- store/expenses/expenses-context.tsx
+++ store/expenses/expenses-context.tsx
@@ -1,12 +1,14 @@
-import {createContext, ReactNode, useReducer} from "react";
+import {createContext, ReactNode, useEffect, useReducer, useState} from "react";
 import {ExpenseInterface} from "../../types/expense";
 import {ExpenseContextInterface} from "../../types/context-api";
 import expensesReducer from "./expenses-reducer";
+import {findAllExpenses} from "../../service/expense";
 
 export const ExpensesContext = createContext<ExpenseContextInterface | null>(null)
 
 function ExpensesContextProvider({children}: Props) {
   const [expensesState, dispatch] = useReducer(expensesReducer, [])
+  const [loading, setLoading] = useState(false)
 
   function readExpense(expenses: ExpenseInterface[]) {
     dispatch({type: 'READ', payload: expenses})
@@ -24,12 +26,40 @@ function ExpensesContextProvider({children}: Props) {
     dispatch({type: 'UPDATED', payload: expense})
   }
 
+  useEffect(() => {
+    async function fetchExpenses() {
+      setLoading(true)
+      try {
+        const expensesMapped: ExpenseInterface[] = []
+        const response = await findAllExpenses()
+        Object.keys(response.data).forEach(key => {
+          const expense: ExpenseInterface = {
+            id: key,
+            date: new Date(response.data[key].date),
+            description: response.data[key].description,
+            amount: response.data[key].amount,
+          }
+          expensesMapped.push(expense)
+
+          readExpense(expensesMapped)
+        })
+      } catch (e) {
+        console.log(e)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchExpenses()
+  }, []);
+
   return (
     <ExpensesContext.Provider value={{
       expenses: expensesState,
       addExpense,
       deleteExpense,
-      updateExpense
+      updateExpense,
+      loading
     }}>
       {children}
     </ExpensesContext.Provider>
