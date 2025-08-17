02085950233ca798fcb15507c1277bb4b84648c6
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-17 14:48:01 +0330

    feat(service): add deleteExpense and updateExpense functions
    Introduced `deleteExpense` and `updateExpense` functions to the service layer for managing expense deletion and updates. Enhanced `findAllExpenses` with more specific response typings for improved type safety.

--- service/expense.ts
+++ service/expense.ts
@@ -1,11 +1,18 @@
-import {get, post} from "./http";
-import {ExpenseCreateResponse, ExpenseInterface} from "../types/expense";
-import {AxiosResponse} from "axios";
+import {del, get, post, put} from "./http";
+import {ExpenseCreateResponse, ExpenseInterface, ExpensesFindAllResponse} from "../types/expense";
 
 export function createExpense(expense: Omit<ExpenseInterface, 'id'>) {
   return post<ExpenseCreateResponse, Omit<ExpenseInterface, 'id'>>('/expenses.json', expense)
 }
 
 export function findAllExpenses() {
-  return get('/expenses.json')
+  return get<ExpensesFindAllResponse>('/expenses.json')
+}
+
+export function deleteExpense(id: string) {
+  return del(`/expenses/${id}.json`)
+}
+
+export function updateExpense(id: string, expense: Omit<ExpenseInterface, 'id'>) {
+  return put(`/expenses/${id}.json`, expense)
 }
