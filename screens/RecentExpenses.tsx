f60852c06368debb42166457754569aa1cf93ef3
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-08-17 15:06:47 +0330

    feat(screens): add loading state handling in RecentExpenses
    Introduced a loading state in the `RecentExpenses` screen using the `LoadingOverlay` component. Added error handling for missing `ExpensesContext`. Filters and displays expenses for the last 7 days only after the loading state resolves.

--- screens/RecentExpenses.tsx
+++ screens/RecentExpenses.tsx
@@ -2,10 +2,14 @@ import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import {useContext} from "react";
 import {ExpensesContext} from "../store/expenses/expenses-context";
 import {getDateMinusDays} from "../utils/date";
+import LoadingOverlay from "../components/common/LoadingOverlay";
 
 function RecentExpenses() {
   const expensesContext = useContext(ExpensesContext)
-  const expenses = expensesContext ? expensesContext.expenses : []
+  if (!expensesContext) {
+    throw new Error('ExpensesContext is not provided')
+  }
+  const {expenses, loading} = expensesContext
   const recentExpenses = expenses
     .filter((expense) => {
       const today = new Date()
@@ -13,6 +17,10 @@ function RecentExpenses() {
 
       return expense.date > dateSevenDaysAgo
     })
+
+  if (loading) {
+    return <LoadingOverlay />
+  }
   return <ExpensesOutput
     expenses={recentExpenses}
     periodName="Last 7 Days"
